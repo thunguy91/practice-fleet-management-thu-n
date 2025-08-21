9ebea4d3589b7e03d98ba281f826eacb3e30aed0
Thu Nguyen <thunguy91@gmail.com>
2025-08-20 22:04:25 -0400

    Updated to add instructions.

--- practice-fleet-management-thu-n.js
+++ practice-fleet-management-thu-n.js
@@ -1,3 +1,8 @@
+// TASK 1: Define the Vehicle Class.
+//     ○ The constructor should include properties: type, make, model, year,
+//          and mileage.
+//     ○ Use default values for mileage (e.g., 0).
+
 class Vehicle {
     constructor(type, make, model, year, mileage = 0) {
         this.type = type;
@@ -6,6 +11,13 @@ class Vehicle {
         this.year = year;
         this.mileage = mileage;
     }
+
+    //TASK 2: Add Methods.
+    //     ○ drive(distance): Increases the vehicle’s mileage by the given
+    //          distance (in kilometers or miles).
+    //     ○ getDetails(): Returns a formatted string describing the vehicle’s
+    //          details.
+
     drive(distance) {
         this.mileage += distance;
     }
@@ -13,6 +25,14 @@ class Vehicle {
         return `The ${this.make} ${this.model} is a ${this.type} made in ${this.year} and has ${this.mileage} miles on it.`;
     }
 }
+
+// TASK 3: Create and Use Vehicle Objects
+//      ○ Instantiate at least three vehicle objects with different properties (e.g.,
+//          a car, a truck, and a motorcycle).
+//      ○ Use the drive method to simulate trips and update mileage.
+//      ○ Use the getDetails method to print each vehicle’s updated
+//          information.
+
 let car = new Vehicle("sedan", "Toyota", "Camry", 2023, 17000);
 console.log(car);
 let truck = new Vehicle("truck", "Ford", "F150", 2025, 300);
